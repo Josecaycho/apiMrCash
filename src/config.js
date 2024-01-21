7b88e14157d6ad8b6aae20340b99ca16f72e73a6
Jose Caycho <jose30caycho@gmail.com>
2024-01-21 17:06:05 -0500

    ddd

--- src/config.js
+++ src/config.js
@@ -6,5 +6,5 @@ export default {
     host: process.env.HOST || "",
     database: process.env.DATABASE || "",
     user: process.env.USER || "",
-    password: ""
+    password: process.env.PASSWORD || "",
 };
