32bf9293d587fea5c64a9ab34ce96c5699af6df8
Jose Caycho <jose30caycho@gmail.com>
2024-01-21 22:44:46 -0500

    asdsad

--- src/database/database.js
+++ src/database/database.js
@@ -2,10 +2,10 @@ import mysql from "promise-mysql";
 import config from "./../config.js";
 
 const connection = mysql.createConnection({
-    host: "dmrcash.c3iykkqesh1q.us-east-1.rds.amazonaws.com",
-    database: "mrcash",
-    user: "admin",
-    password: "Mrcashproyect"
+    host: 'dmrcash.c3iykkqesh1q.us-east-1.rds.amazonaws.com',
+    database: 'mrcash',
+    user: 'admin',
+    password: 'Mrcashproyect'
 });
 
 const getConnection = () => {
