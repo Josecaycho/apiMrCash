1e92526e9e18e5b9a6025f0a516b12753a36b942
Jose Caycho <jose30caycho@gmail.com>
2024-01-21 22:40:03 -0500

    sss

--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@ import { config } from "dotenv";
 config();
 
 export default {
-    host: process.env.HOST || "",
-    database: process.env.DATABASE || "",
-    user: process.env.USER || "",
-    password: process.env.PASSWORD || "",
+    host: process.env.HOST || "bdmrcash.c3iykkqesh1q.us-east-1.rds.amazonaws.com",
+    database: process.env.DATABASE || "mrcash",
+    user: process.env.USER || "admin",
+    password: process.env.PASSWORD || "Mrcashproyect",
 };
